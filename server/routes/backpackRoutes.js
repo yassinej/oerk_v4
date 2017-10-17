dcf781dc26a298dd722b8bd0a5862aa27fa390fe
dev <yassinej@gmail.com>
2017-10-17 17:44:01 +0300

    Added find and update route for backpacks

--- server/routes/backpackRoutes.js
+++ server/routes/backpackRoutes.js
@@ -52,7 +52,27 @@ module.exports = app => {
 			}
 		}
 	);
-	//logged Admin can fetch backpack
+	//logged user can fetch a user specific backpack
+	app.get(
+		'/api/backpacks/user/:id',
+		Authentication.requireLogin,
+		async (req, res) => {
+			const id = req.params.id;
+			console.log('Id is:', id);
+			try {
+				const fetchedBackpack = await Backpack.findOne({ user_id: id });
+				if (!fetchedBackpack)
+					res.status(401).json({ error: 'fetching backpack failed' });
+				res.status(200).json({
+					message: 'Backpack fetched',
+					backpack: fetchedBackpack
+				});
+			} catch (e) {
+				res.status(400).json({ error: 'Unexpected error', stack: e });
+			}
+		}
+	);
+	//logged user can fetch a specific backpack
 	app.get(
 		'/api/backpacks/:id',
 		Authentication.requireLogin,
@@ -72,20 +92,26 @@ module.exports = app => {
 			}
 		}
 	);
-	//logged Admin can fetch backpack
-	app.get(
-		'/api/backpacks/:id',
+
+	//logged user can update and existing backpack
+	app.post(
+		'/api/backpacks/:id/update',
 		Authentication.requireLogin,
 		async (req, res) => {
+			console.log(req);
 			const id = req.params.id;
+			const backpack = req.body;
 			console.log('Id is:', id);
 			try {
-				const fetchedBackpack = await Backpack.findOne({ _id: id });
-				if (!fetchedBackpack)
-					res.status(401).json({ error: 'fetching backpack failed' });
+				const updatedBackpack = await Backpack.findOneAndUpdate(
+					{ _id: id },
+					backpack
+				);
+				if (!updatedBackpack)
+					res.status(401).json({ error: 'updating backpack failed' });
 				res.status(200).json({
-					message: 'Backpack fetched',
-					backpack: fetchedBackpack
+					message: 'Backpack updated',
+					backpack: updatedBackpack
 				});
 			} catch (e) {
 				res.status(400).json({ error: 'Unexpected error', stack: e });
