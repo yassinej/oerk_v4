6ef256f4e70fedf74c3743ce07721a89770a4c5f
dev <yassinej@gmail.com>
2017-10-19 21:01:17 +0300

    Issue6: Implemented a Basic Landing Component

--- client/src/js/components/Landing.js
+++ client/src/js/components/Landing.js
@@ -4,8 +4,20 @@ class Landing extends Component {
 	render() {
 		return (
 			<div>
-				<h1>Landing Page</h1>
-				<h3>Welcome to OERK V4</h3>
+				<div>
+					<h1>Welcome to OERK V4</h1>
+				</div>
+				<div>
+					<div>Step1</div>
+					<div>Step2</div>
+					<div>Step3</div>
+				</div>
+				<div>
+					<h2>Get Started</h2>
+					<button>
+						<a href="/auth/google">SignIn with Google</a>
+					</button>
+				</div>
 			</div>
 		);
 	}
